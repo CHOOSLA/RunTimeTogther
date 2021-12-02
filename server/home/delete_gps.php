7e46bbcae90c054776ca3760c79514f078eea2f0
Choo Chang Woo <cnckddn0146@gmail.com>
2021-12-03 06:55:25 +0900

    맵완성

---
+++ server/home/delete_gps.php
@@ -0,0 +1,21 @@
+<?php
+include 'db.php';
+// Getting the received JSON into $json variable.
+$json = file_get_contents('php://input');
+
+// Decoding the received JSON and store into $obj variable.
+$obj = json_decode($json,true);
+
+$userid = $obj['userid'];
+
+//Applying User Login query with email and password.
+$deleteQuery = "DELETE FROM currentgps where userid='$userid';";
+
+$result = mysqli_query($db,$deleteQuery);
+
+$mgs = '삭제 성공';
+$success = json_encode($mgs);
+echo $success;
+
+mysqli_close($db);
+?>
